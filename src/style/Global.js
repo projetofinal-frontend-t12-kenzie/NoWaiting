0e134ce9201bbc9a26337076db7fc89d6b641ee9
Guilherme Bernardo <guilhermebernardo@id.uff.br>
2022-08-30 16:52:26 -0300

    feat: estabelicimento de padrao

---
+++ src/style/Global.js
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+:root{
+    ${'' /* --color-primary:;
+    --color-primary-focused:;
+    --color-primary-negative:;
+    --grey-4:;
+    --grey-3:;
+    --grey-2:;
+    --grey-1:;
+    --grey-0:;
+    --success: ;
+    --negative:;
+    --font: Inter;
+    --title: bold 21px;
+    --headline: 12px;
+    --headline-bold: bold 12px;
+    --headline-italic: italic 12px;
+    --border-radius: 4px; */}
+}
+`
