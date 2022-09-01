8cfa706675c43e723db23332a8cd1e1a42966766
giovannabeathryce <giovannabeathryce.pso@gmail.com>
2022-09-01 18:28:20 -0300

    feat: Adição e estilização dos componentes BoxRecomendados / CardProdutos

---
+++ src/components/DashboarCliente/BoxRecomendados/BoxRecomendado.jsx
@@ -0,0 +1,15 @@
+import { ContainerRecomendados } from "./BoxRecomendado.style";
+
+const ProdutosRecomendados = () => {
+  return (
+    <ContainerRecomendados>
+      <h2>Recomendado</h2>
+      <img
+        src="https://images.pexels.com/photos/574111/pexels-photo-574111.jpeg?auto=compress&cs=tinysrgb&w=1600"
+        alt=""
+      />
+    </ContainerRecomendados>
+  );
+};
+
+export default ProdutosRecomendados;
