cee38fd7f500d5036343bd5fa7d4cc2d12816ab8
thiagonazarko <thnzk1@gmail.com>
2022-09-02 16:35:06 -0300

    Fixes: EyeSlyte e SelectPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import "./App.css";
 import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
-import SelectPage from "./pages/SelectPage/SelectPage";
 
 function App() {
   return (
     <div className="App">
-      {/* <SelectPage /> */}
-      {/* <Login /> */}
+      <Login />
       {/* <Register /> */}
     </div>
   );
