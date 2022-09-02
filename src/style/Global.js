7587de3be9b8aa48c97e02e401f831354dcdb9b4
Marcos Vinicius Damaceno <marcosvinicius.mmn@gmail.com>
2022-09-02 14:11:31 -0300

    feat: Dashboard chef

--- src/style/Global.js
+++ src/style/Global.js
@@ -1,22 +1,68 @@
 import { createGlobalStyle } from "styled-components";
-
 export default createGlobalStyle`
+html, body, div, span, applet, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+a, abbr, acronym, address, big, cite, code,
+del, dfn, em, img, ins, kbd, q, s, samp,
+small, strike, strong, sub, sup, tt, var,
+b, u, i, center,
+dl, dt, dd, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, embed,
+figure, figcaption, footer, header, hgroup,
+menu, nav, output, ruby, section, summary,
+time, mark, audio, video {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    font-size: 100%;
+    font: inherit;
+    vertical-align: baseline;
+}
+/* HTML5 display-role reset for older browsers */
+article, aside, details, figcaption, figure,
+footer, header, hgroup, menu, nav, section {
+    display: block;
+}
+body {
+    line-height: 1;
+}
+ol, ul {
+    list-style: none;
+}
+blockquote, q {
+    quotes: none;
+}
+blockquote:before, blockquote:after,
+q:before, q:after {
+    content: '';
+    content: none;
+}
+table {
+    border-collapse: collapse;
+    border-spacing: 0;
+}
+-webkit-tap-highlight-color: transparent;
+a:-webkit-any-link {
+    color: none;
+    text-decoration: none;
+}
 :root{
-    ${'' /* --color-primary:;
-    --color-primary-focused:;
-    --color-primary-negative:;
-    --grey-4:;
-    --grey-3:;
-    --grey-2:;
-    --grey-1:;
-    --grey-0:;
-    --success: ;
-    --negative:;
+    --color-primary: #FA7B12;
+    --color-primary-focused:#FFA75E;
+    --grey-3:#141414;
+    --grey-2:#313131;
+    --grey-1:#D9D9D9;
+    --grey-0:#FFFFFF;
+    --grey-transparent: rgba(255, 255, 255, 0.4);
+    --success:#4CAF50;
+    --negative:#BF2D2D;
     --font: Inter;
     --title: bold 21px;
     --headline: 12px;
     --headline-bold: bold 12px;
     --headline-italic: italic 12px;
-    --border-radius: 4px; */}
+    --border-radius: 4px;
 }
-`
+`;
