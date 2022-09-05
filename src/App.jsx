2d06029cfbcf05e7c3958cfe594cf9e89ab5c14e
victorgois <victoraugusto340@gmail.com>
2022-09-05 12:17:03 -0300

    att

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import "./App.css";
 import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
-import SelectPage from "./pages/SelectPage/SelectPage";
 
 function App() {
   return (
     <div className="App">
-      {/* <SelectPage /> */}
       {/* <Login /> */}
       {/* <Register /> */}
     </div>
