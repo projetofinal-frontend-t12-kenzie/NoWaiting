f3df20a33b50da3ad988f7b7256c4103c05bdeab
Marcos Vinicius Damaceno <marcosvinicius.mmn@gmail.com>
2022-09-06 11:42:32 -0300

    fix: alterando width header

--- src/components/DashboardRest/HeaderRest.style.js
+++ src/components/DashboardRest/HeaderRest.style.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const HeaderRest = styled.div`
   display: flex;
   justify-content: space-between;
-  width: 85%;
+  width: 95%;
 
   @media screen and (max-width: 426px) {
     align-items: center;
